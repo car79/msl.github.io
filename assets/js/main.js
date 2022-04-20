eb7dfbbbb7adfeb73407deb748cf718743e72c73
Your Name <you@example.com>
2022-04-20 18:25:44 +0200

    Add wow.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,26 @@
-const btn_scrool_to_up = document.querySelector(".btn-scrool-to-up");
-console.log(btn_scrool_to_up.style.display);
+/*--------------------
+            wow js init
+        ---------------------*/
+var wow = new WOW({
+  boxClass: "wow", // animated element css class (default is wow)
+  animateClass: "animated", // animation css class (default is animated)
+  offset: 0, // distance to the element when triggering the animation (default is 0)
+  mobile: true, // trigger animations on mobile devices (default is true)
+  live: true, // act on asynchronously loaded content (default is true)
+  callback: function (box) {
+    // the callback is fired every time an animation is started
+    // the argument that is passed in is the DOM node being animated
+  },
+  scrollContainer: null, // optional scroll container selector, otherwise use window
+});
+wow.init();
 
+const btn_scrool_to_up = document.querySelector(".btn-scrool-to-up");
 const scrool = () => {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    btn_scrool_to_up.style.display = "block";
-    btn_scrool_to_up.style.opacity = "1";
+    btn_scrool_to_up.classList.add("show");
   } else {
-    btn_scrool_to_up.style.display = "none";
-    btn_scrool_to_up.style.opacity = "0";
+    btn_scrool_to_up.classList.remove("show");
   }
 };
 window.onscroll = () => scrool();
